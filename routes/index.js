52ca9e407c11597e22a57b74a802eff5bbe977c3
vollantre <rinbinary@gmail.com>
2020-02-02 21:21:30 -0400

    finish login view

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.redirect('/sign-up')
-})
+router.get('/', (req, res) => res.redirect('/sign-up'))
 
 router.get('/sign-up', userController.register_get)
 
 router.post('/sign-up', userController.register_post)
+
+router.get('/login', (req, res) => res.redirect('/log-in'))
 
 router.get('/log-in', userController.login_get)
 
